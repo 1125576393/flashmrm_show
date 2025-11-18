20e47c07c95296e1aca82e0c09cc1c4d2cec834a
1125576393 <1125576393@qq.com>
2025-11-18 17:30:12 +0800

    Update FlashMRM_streamlit_show.py

--- FlashMRM_streamlit_show.py
+++ FlashMRM_streamlit_show.py
@@ -480,7 +480,7 @@ with col_b:
         unsafe_allow_html=True
     )
     if selected_mode == "Single mode":
-        inchikey_input = st.txt_input(
+        inchikey_input = st.text_input(
             "Single mode:",
             key="inchikey_input_active",
             value=st.session_state.get("inchikey_value", ""),
@@ -760,6 +760,7 @@ if st.session_state.calculation_complete:
     st.success(f"Calculation complete ✅ | Successfully processed: {success_count}| Overall processing: {len(result_df)}")
 else:
     st.warning("No results generated. Please check your input data or parameter configuration！")
+
 
 
 
