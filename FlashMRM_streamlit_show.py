9b61e17c31d406bd2747f3a54a26cf8c52a5cc59
1125576393 <1125576393@qq.com>
2025-11-20 12:37:18 +0800

    Update FlashMRM_streamlit_show.py

--- FlashMRM_streamlit_show.py
+++ FlashMRM_streamlit_show.py
@@ -616,7 +616,7 @@ with st.expander("Parameter Setting"):
         with col4:
             rt_bg = "#F0F5FF"   
             rt_tolerance = st_yled.number_input(
-                "Retention time tolerance:",
+                "Retention\u00A0time\u00A0tolerance:",
                 min_value=0.0,
                 max_value=10.0,
                 value=2.0,
@@ -772,6 +772,7 @@ if st.session_state.calculation_complete:
     success_count = success_conditions.sum()  # 用sum()统计True的数量，避免len()的歧义
         
     st.success(f"Calculation complete ✅ | Successfully processed: {success_count}| Overall processing: {len(result_df)}")
+
 
 
 
